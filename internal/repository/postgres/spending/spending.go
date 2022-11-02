e5cd391981f382ec3332da61c614d0f0fcbf50db
Скубач Сергей <s.skubach@e-comexpert.com>
2022-11-03 00:07:13 +0300

    [GOHW-6]: add context to report events cache

--- internal/repository/postgres/spending/spending.go
+++ internal/repository/postgres/spending/spending.go
@@ -90,8 +90,8 @@ func (s Spending) Report(ctx context.Context, f1, f2 time.Time, rates rates.Clie
 		Key:   keyCacheReport,
 		Value: &events,
 		TTL:   time.Minute * 10,
-	}, func(*cache.Item) (interface{}, error) {
-		if err = s.db.SelectContext(ctx, &events, queryReport,
+	}, func(ci *cache.Item) (interface{}, error) {
+		if err = s.db.SelectContext(ci.Ctx, &events, queryReport,
 			f1.Format("2006-01-02"), f2.Format("2006-01-02")); err != nil {
 			return nil, errors.Wrap(err, "select report")
 		}
